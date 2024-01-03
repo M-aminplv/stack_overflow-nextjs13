8a3f544c06a084966743f62c77ce58f2a04bdddf
Amin <pahlavanmohammadamin@gmail.com>
2024-01-03 11:46:48 +0330

    question card done

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,44 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimestamp = (createdAt:Date): string => {
+	const currentDate = new Date();
+	const timeDifference = currentDate.getTime() - createdAt.getTime();
+
+	const seconds = Math.floor(timeDifference / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+	const months = Math.floor(days / 30);
+	const years = Math.floor(months / 12);
+
+	if (years > 0) {
+		return `${years} year${years > 1 ? "s" : ""} ago`;
+	} else if (months > 0) {
+		return `${months} month${months > 1 ? "s" : ""} ago`;
+	} else if (days > 0) {
+		return `${days} day${days > 1 ? "s" : ""} ago`;
+	} else if (hours > 0) {
+		return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+	} else if (minutes > 0) {
+		return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+	} else {
+		return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+	}
+};
+
+export const formatAndDivideNumber = (number: number):string => {
+	if (number >= 1000000) {
+		const result = (number / 1000000).toFixed(1);
+		return `${result}M`;
+	} else if (number >= 1000) {
+		const result = (number / 1000).toFixed(1);
+		return `${result}k`;
+	} else {
+		return `${number}`;
+	}
+};
+
+
+
